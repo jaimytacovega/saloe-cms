4fafa9b5f0902da902426980cb900dca85a6ca79
Jaimy Taco <jaimy.taco.vega@gmail.com>
2025-08-18 02:26:26 -0500

    feat: go back to categories when updating/creating

--- src/features/cms/features/Category/components/CategoryToolbox/index.js
+++ src/features/cms/features/Category/components/CategoryToolbox/index.js
@@ -4,21 +4,28 @@ import { html } from 'saloe/html'
 const CategoryToolbox = ({
     categoryId,
 }) => {
-    return html`
-        <form>
-            <inputgroup>
-                <img loading="lazy" src="/img/icon/search-black.svg" width="16" height="16" alt="search">
-                <input type="search" placeholder="Buscar"/>
-            </inputgroup>
-            <inputgroup>
-                <button class="Button PrimaryButton PrimaryGray">Filtrar</button>
-                <button class="Button PrimaryButton PrimaryGray">Ir a</button>
-                <button class="Button PrimaryButton PrimaryGray">Ordenar</button>
-                <hr>
-                <a href="/cms/categorias/crear" class="Button PrimaryButton PrimaryBlue">Crear</a>
-            </inputgroup>
-        </form>
-    `
+    return categoryId
+        ? html`
+            <a href="/cms/categorias" class="Button PrimaryButton PrimaryGray">
+                <img src="/img/icon/corner-up-left-gray-1.svg" width="18" height="18" alt="go back">
+                <span>Regresar a categorías</span>
+            </a>
+        `
+        : html`
+            <form>
+                <inputgroup>
+                    <img loading="lazy" src="/img/icon/search-black.svg" width="16" height="16" alt="search">
+                    <input type="search" placeholder="Buscar"/>
+                </inputgroup>
+                <inputgroup>
+                    <button class="Button PrimaryButton PrimaryGray">Filtrar</button>
+                    <button class="Button PrimaryButton PrimaryGray">Ir a</button>
+                    <button class="Button PrimaryButton PrimaryGray">Ordenar</button>
+                    <hr>
+                    <a href="/cms/categorias/crear" class="Button PrimaryButton PrimaryBlue">Crear</a>
+                </inputgroup>
+            </form>
+        `
 }
 
 export default CategoryToolbox
