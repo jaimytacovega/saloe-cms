ab29bc76861d5cb02b6ab82a1df4900f12af3180
Jaimy Taco <jaimy.taco.vega@gmail.com>
2025-08-18 13:27:13 -0500

    feat: empty-table

--- src/shared/components/Table/index.js
+++ src/shared/components/Table/index.js
@@ -1,24 +1,47 @@
 import { html } from 'saloe/html'
 
 
-const Table = ({
-    rows,
-}) => {
+const EmptyTable = () => {
     return html`
-        <table>
+        <table empty>
             <tbody>
-                ${
-                    (rows ?? []).map((row) => html`
-                        <tr>
-                            <td>
-                                ${row}
-                            </td>
-                        </tr>
-                    `).join('')
-                }
+                <tr>
+                    <td>
+                        <header>
+                            <h5>Aun no se registran datos</h5>
+                            <p>Crea nuevos registros y gestionalos desde esta sección</p>
+                        </header>
+                        <a href="/cms/categorias/crear" class="Button PrimaryButton PrimaryBlue">
+                            <img loading="lazy" src="/img/icon/plus-white.svg" width="16" height="16" alt="Crear">
+                            <span>Crear</span>
+                        </a>
+                    </td>
+                </tr>
             </tbody>
         </table>
     `
+}
+
+const Table = ({
+    rows = [],
+}) => {
+    return rows.length > 0
+        ? html`
+            <table>
+                <tbody>
+                    ${
+                        (rows ?? []).map((row) => html`
+                            <tr>
+                                <td>
+                                    ${row}
+                                </td>
+                            </tr>
+                        `).join('')
+                    }
+                </tbody>
+            </table>
+        `
+        : EmptyTable()
 }
 
 export default Table
