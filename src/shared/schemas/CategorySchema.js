ce5f5df0fe7291300c39da10ff39973d354625b5
Jaimy Taco <jaimy.taco.vega@gmail.com>
2025-09-22 22:04:02 -0500

    fix: DeleteCategorySchema

--- src/shared/schemas/CategorySchema.js
+++ src/shared/schemas/CategorySchema.js
@@ -57,7 +57,8 @@ const UpdateCategorySchema = z.object({
 
 const DeleteCategorySchema = z.object({
     id: z.string().min(1, 'El id es obligatorio').trim(),
-    path: z.string().min(1, 'La ruta es obligatoria').trim(),
+    path: z.string().min(1, 'La ruta es obligatoria').trim().optional(),
+    catalogPaths: z.array(z.string()).min(1, 'Las rutas de los catálogos son obligatorias').trim().optional(),
 })
 
 export {
