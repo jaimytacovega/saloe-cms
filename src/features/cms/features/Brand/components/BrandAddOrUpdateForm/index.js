1cce0281ee608bbde551f0b2196c9dcd08c2266d
Jaimy Taco <jaimy.taco.vega@gmail.com>
2025-10-13 13:28:26 -0500

    fix: go-back button in brand-form

--- src/features/cms/features/Brand/components/BrandAddOrUpdateForm/index.js
+++ src/features/cms/features/Brand/components/BrandAddOrUpdateForm/index.js
@@ -86,13 +86,12 @@ const BrandAddOrUpdateForm = async ({
                     </fieldset>
                 </div>
                 <inputgroup>
+                    <a href="${listUrl}?${searchParams?.toString()}" class="Button PrimaryButton PrimaryGray">
+                        <img src="/img/icon/corner-up-left-black.svg" width="18" height="18" alt="go back">
+                    </a>
                     ${
                         brandId === 'new'
-                            ? html`
-                                <a href="${listUrl}?${searchParams?.toString()}" class="Button PrimaryButton PrimaryGray">
-                                    <img src="/img/icon/corner-up-left-black.svg" width="18" height="18" alt="go back">
-                                </a>
-                            `
+                            ? ''
                             : html`
                                 <button popovertarget="DeleteBrandDialog-${brandId}" type="button" class="Button PrimaryButton PrimaryGray">
                                     <img src="/img/icon/trash-black.svg" width="18" height="18" alt="trash">
