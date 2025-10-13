5c2f16a9ee5c6e655a51b2d092f6d64f18b28f26
Jaimy Taco <jaimy.taco.vega@gmail.com>
2025-10-12 22:51:07 -0500

    fix: clean code in CmsToolbox

--- src/features/cms/components/CmsToolbox/index.js
+++ src/features/cms/components/CmsToolbox/index.js
@@ -11,23 +11,6 @@ const CmsToolbox = ({
     title,
     actions,
 }) => {
-    // return id
-    //     ? html`
-    //         <a href="${listUrl}?${searchParams?.toString()}" class="Button PrimaryButton PrimaryGray">
-    //             <img src="/img/icon/corner-up-left-gray-1.svg" width="18" height="18" alt="regresar">
-    //             <span>Regresar a ${title}</span>
-    //         </a>
-    //     `
-    //     : html`
-    //         ${
-    //             CmsSearchForm({
-    //                 createUrl,
-    //                 searchParams,
-    //             })
-    //         }
-    //         ${actions}
-    //     `
-
     return html`
         ${
             CmsSearchForm({
