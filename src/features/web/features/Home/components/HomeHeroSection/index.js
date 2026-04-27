bfdb0094b930761e85b4eaed78d6d97d2d3257ac
Jaimy Taco <jaimy.taco.vega@gmail.com>
2026-04-27 16:14:53 -0500

    fix: default banner

--- src/features/web/features/Home/components/HomeHeroSection/index.js
+++ src/features/web/features/Home/components/HomeHeroSection/index.js
@@ -8,21 +8,29 @@ import WebHeroSection from '@/features/web/components/WebHeroSection'
 
 const HomeHeroSection = async () => {
     const { data: banners } = await WebHook.useListPublishedBanners({ ttl: 10_000 })
-    const banner = banners?.at(0)
+
+    const DEFAULT_BANNER = {
+        pretitle: 'Productos para constructoras y ferreterias',
+        title: 'Cotiza tu pedido con nosotros',
+        description: 'De la manera más rapida y sencilla, adjunta tu pedido, dejando tus datos y nosotros te contactaremos.',
+        image: '/img/thumbnail/hero-1.png',
+    }
+
+    const banner = banners?.at(0) ?? DEFAULT_BANNER
 
     return html`
         ${
             WebHeroSection({
-                preTitle: banner?.pretitle ?? 'Productos para constructoras y ferreterias',
+                preTitle: banner.pretitle,
                 title: html`
-                    <h1>${banner?.title ?? 'Cotiza tu pedido con nosotros'}</h1>
+                    <h1>${banner.title}</h1>
                 `,
-                description: banner?.description ?? 'De la manera más rapida y sencilla, adjunta tu pedido, dejando tus datos y nosotros te contactaremos.',
+                description: banner.description,
                 toolbox: html`
                     <button class="Button PrimaryButton PrimaryBlue">Cotizar pedido</button>
                 `,
                 isHeroImage: true,
-                thumbnail: banner?.image.downloadURL ?? '/img/thumbnail/hero-1.png',
+                thumbnail: banner.image.downloadURL,
             })
         }
     `
